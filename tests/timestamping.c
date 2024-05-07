90bc48d6d229c6e2ec5dcd399fb7a7a8b173d67c
Rishitha Minol <rishithaminol@gmail.com>
2024-05-07 20:14:26 +0530

    timespec_iso8601 function test completed

--- tests/timestamping.c
+++ tests/timestamping.c
@@ -4,6 +4,11 @@
  * 
  * Include timestamping section of the program and compile this.
  * Then test whether that functionality works perfectly.
+ * 
+ * Generate hand made timestamp.
+ * Pass the timestamp into timespec_iso8601 and get the output string.
+ * Compare the integers with the returned string.
+ * If it equals return 0
 */
 
 #include <stdio.h>
@@ -18,19 +23,17 @@
 int main(void)
 {
     char timestr[32];
-    struct timespec tms;
-    int64_t nanos, nanos_part;
+    struct timespec tms1, tms2;
+    int ret = 0;
 
-    if(clock_gettime(CLOCK_REALTIME, &tms)) {
-        return -1;
-    }
+    tms1.tv_sec = 1555817400;
+    tms1.tv_nsec = 846599595;
+
+    tms2.tv_sec = 1555817400;
+    tms2.tv_nsec = 44817714;
 
     /*
-    Generate timestamp
-    pass that into timespec_iso8601 and get the output string
-    Compare the integers with the returned string
-    If it equals return 0
-    */
+    This section used to visualize how timestamping data is displayed.
 
     nanos = tms.tv_sec * 1000000000;
     printf("timestamp: %" PRIu64 "\n", nanos);
@@ -40,15 +43,25 @@ int main(void)
 
     nanos += nanos_part;
     printf("timestamp: %" PRIu64 "\n", nanos);
+    */
 
+    set_timezone("UTC");
 
-    if (timespec_iso8601(timestr, sizeof(timestr), &tms) != 0) {
-        printf("timespec2str failed!\n");
+    if (timespec_iso8601(timestr, sizeof(timestr), &tms1) != 0) {
+        printf("timespec_iso8601 failed!\n");
         return EXIT_FAILURE;
-    } else {
-        printf("Time: %s\n", timestr);
-        return EXIT_SUCCESS;
     }
 
-    return 0;
+    if (strcmp("2019-04-21T03:30:00.846599595Z", timestr) != 0)
+        ret = 1;
+
+    if (timespec_iso8601(timestr, sizeof(timestr), &tms2) != 0) {
+        printf("timespec_iso8601 failed!\n");
+        return EXIT_FAILURE;
+    }
+
+    if (strcmp("2019-04-21T03:30:00.044817714Z", timestr) != 0)
+        ret = 1;
+
+    return ret;
 }
